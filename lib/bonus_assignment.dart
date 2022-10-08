6fc3a56aa307b18540cd8ebbd6f2cb106554b06b
ihnaqi <ihnaqi@gmail.com>
2022-10-08 20:13:23 +0500

    Bonus Assigment 1 Completed

--- lib/bonus_assignment.dart
+++ lib/bonus_assignment.dart
@@ -8,11 +8,110 @@ class BonusAssignment extends StatefulWidget {
 }
 
 class _BonusAssignmentState extends State<BonusAssignment> {
+  final usernameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: const Text("Bonus Assignemt"),
+        title: const Text("Bonus Assignemt (Login Page)"),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Image(
+              image: NetworkImage(
+                  "https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg"),
+              width: 200,
+              height: 200,
+            ),
+            Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Padding(
+                        padding:
+                            const EdgeInsets.only(left: 15, right: 15, top: 5),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            labelText: 'Email',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Stack(
+                      alignment: const Alignment(0, 0),
+                      children: <Widget>[
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                left: 15, right: 15, top: 5),
+                            child: TextFormField(
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                labelText: 'Your password',
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          right: 15,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('SHOW'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: SizedBox(
+                      height: 50,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        // color: Colors.green,
+                        onPressed: () {},
+                        child: Text(
+                          'Submit',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: const BorderSide(
+                                color: Colors.green,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
